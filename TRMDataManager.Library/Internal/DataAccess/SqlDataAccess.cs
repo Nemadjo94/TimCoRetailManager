2ceb939e59b9ca431c092439cff120e64139ec28
Nemanja Djordjevic <nemadjo94@gmail.com>
2019-12-24 18:27:31 +0100

    Implement User Data acquisition
    -Made a stored procedure to get user data
    -Implemented Dapper (micro orm)
    -Debugged sql connection problem

---
+++ TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDataManager.Library.Internal.DataAccess
+{
+    // Should stay internal because we dont want to allow outside calls
+    internal class SqlDataAccess
+    {
+        public string GetConnectionString(string name)
+        {
+            // Gets the connection string from Web.config
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        // Loads data from db
+        public List<T> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using(IDbConnection connection = new SqlConnection(connectionString))
+            {
+                // Makes a connection, does a query and executes a stored procedure which returns some rows of data
+                List<T> rows = connection.Query<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure).ToList();
+                return rows;
+            }
+        }
+
+        // Saves data to db
+        public void SaveData<T>(string storedProcedure, T parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Execute(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
---
+++ TRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+       // GET: api/User/
+       [HttpGet]
+       [Route("GetById")]
+       public List<UserModel> GetById()
+        {
+            // Get current logged in user id
+            string userId = RequestContext.Principal.Identity.GetUserId();
+
+            UserData data = new UserData();
+
+            var result = data.GetUserById(userId);
+
+            return result;
+        }
+    }
+}
